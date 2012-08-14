74713ebdb22db36c702b30040f76243f6b17b037
tainyu-navi.com <info@tainyu-navi-o.com>
2012-08-14 15:54:27 +0900

    Update src/m.js

--- src/m.js
+++ src/m.js
@@ -1,6 +1,4 @@
-(function(){
-	
-	(function(e){function u(q,a,g,k){var i=a.button||g.button,n=a.url||g.url,o=a.provider_name||g.provider_name,p=a.suggest_notebook||g.suggest_notebook,r=a.content_id||g.content_id,s=a.code||g.code,t=a.title||g.title,m=a.suggest_tags||g.suggest_tags,a=a.styling||g.styling;if("article-clipper"==i||"article-clipper-remember"==i||"article-clipper-jp"==i||"article-clipper-rus"==i||"article-clipper-fr"==i||"article-clipper-es"==i||"article-clipper-de"==i||"article-clipper-vert"==i||"site-mem-32"==i||"site-mem-36"==
+(function(e){function u(q,a,g,k){var i=a.button||g.button,n=a.url||g.url,o=a.provider_name||g.provider_name,p=a.suggest_notebook||g.suggest_notebook,r=a.content_id||g.content_id,s=a.code||g.code,t=a.title||g.title,m=a.suggest_tags||g.suggest_tags,a=a.styling||g.styling;if("article-clipper"==i||"article-clipper-remember"==i||"article-clipper-jp"==i||"article-clipper-rus"==i||"article-clipper-fr"==i||"article-clipper-es"==i||"article-clipper-de"==i||"article-clipper-vert"==i||"site-mem-32"==i||"site-mem-36"==
 			i||"site-mem-22"==i||"site-mem-16"==i)i="http://static.evernote.com/"+i+".png";var d={url:n,providerName:o,suggestNotebook:p,contentId:r,code:s,title:t,suggestTags:m,styling:a};0==k&&e("body").append('<script type="text/javascript" src="http://static.evernote.com/noteit.js"><\/script>');k=e(document.createElement("a")).attr({href:"#"}).click(function(){Evernote&&Evernote.doClip(d);return!1}).append('<img src="'+i+'" alt="Clip to Evernote" style="border: none" />');e(q).html(k)}function n(e){var a=
 			"",g;for(g in e)""!=e[g]&&(a+=" "+g+'="'+e[g]+'"');return a}function p(e){var a="",g;for(g in e)""!=e[g]&&(a+=""!=a?"&amp;":"",a+=g+"="+e[g]+"");return a}function k(e){var a=[[/&/g,"&amp;"],[/</g,"&lt;"],[/>/g,"&gt;"],[/"/g,"&quot;"],[/'/g,"&#039;"]],g;for(g in a)e=e.replace(a[g][0],a[g][1]);return e}function o(e){return encodeURIComponent(e).replace(/[!*'()]/g,function(a){return"%"+a.charCodeAt(0).toString(16)})}e.fn.socialbutton=function(q,a){var a=a||{},g=document.URL,v=document.URL,i=document.URL,
 			w={type0_16:58,type0_20:70,type0_23:76,type1_16:58,type1_20:73,type1_23:78,type2_16:58,type2_20:73,type2_23:78,type3_16:49,type3_20:61,type3_23:64,type4_16:16,type4_22:21,type4_32:32},x={button:"article-clipper",url:"",provider_name:"",suggest_notebook:"",content_id:"",code:"",title:"",suggest_tags:"",styling:""},y=document.URL,r=document.title,s=document.URL,t=document.URL,m=this.size()-1;return this.each(function(d){switch(q){case "mixi_check":var b=a.key||"",h=a.button||"button-1",c=a.url||"";
@@ -15,17 +13,40 @@
 			b+'" style="border: none; vertical-align: text-bottom" /></a></span>');break;case "google_plusone":j=a;if(!(e.browser.msie&&8>parseInt(e.browser.version,10))){c=j.size||j.button||"";f=j.href||j.url||"";b=j.lang||"";h=j.parsetags||"";l=j.callback||"";j=void 0!=j.count?j.count:!0;switch(c){case "small":case "standard":case "medium":break;case "tall":j=!0;break;default:c="standard",j=!0}c=e("<div>").attr({"data-callback":l,"data-count":j?"true":"false","data-href":f,"data-size":c}).addClass("g-plusone");
 		e(this).append(c);d==m&&(d="",""!=b&&(d+='lang: "'+k(b)+'"'),""!=h&&(d=d+(""!=d?",":"")+('parsetags: "'+k(h)+"'")),""!=d&&(d="{"+d+"}"),"undefined"===typeof gapi||"undefined"===typeof gapi.plusone?e("body").append('<script type="text/javascript" src="https://apis.google.com/js/plusone.js">'+d+"<\/script>"):gapi.plusone.go())}break;case "pinterest":b=a.url||t,h=a.button||"horizontal",c=void 0!=a.media?a.media:"",f=void 0!=a.description?a.description:"",b=o(decodeURIComponent(b)),c=o(decodeURIComponent(c)),
 			f=decodeURIComponent(f),b='<a href="http://pinterest.com/pin/create/button/?'+p({url:b,media:c,description:f})+'" class="pin-it-button" count-layout="'+h+'"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>',e(this).html(b),d==m&&e("body").append('<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"><\/script>')}return!0})}})(jQuery);
-	document.write('<style type="text/css">.clearfix:after {content: ".";display: block;height: 0;clear: both;visibility: hidden;}.clearfix {display: inline-block;}/* Hides from IE-mac \*/* html .clearfix {height: 1%;}.clearfix {display: block;}/* End hide from IE-mac */.block div {margin-left : 5px;margin-right: 0;float: left;}</style>');
+	document.write('<style type="text/css">.clearfix:after{content:".";display:block;height:0;clear:both;visibility:hidden;}.clearfix{display:inline-block;}/*HidesfromIE-mac\*/*html.clearfix{height:1%;}.clearfix{display:block;}/*EndhidefromIE-mac*/.blockdiv{margin-left:5px;margin-right:0;float:left;}.socialbutton-wrapdiv{display:inline;float:left;}.socialbutton-clear-botn{clear:both;height:0;margin:0;padding:0;border:none;}</style>');
 	$(function() {
+		var data=[
+			{
+				type : 'twitter',
+				options:{
+					button : 'none',
+					text:   '関東ＮＯ．１高収入サイト【はじめての体験入店】は、'+
+							'未経験の誰にでも出来るトーク倶楽部から高級風俗迄、'+
+							'高収入求人や風俗アルバイトを簡単一発検索♪サクッと稼げる♪'+'' +
+							'超高収入が即金可能♪【全て営業届出確認書有良店】だから安心安全です。'
+				}
+			},
+			{
+				type : 'facebook_like',
+				options : {
+					button : 'button_count'
+				}
+			}
+		];
+		var socialbutton=$('#socialbutton,.socialbutton');
+		var elem=$('<div class="socialbutton-wrap"/>');
+		var cl=function(a,b){
+			console.log(a,b);
+		};
 		$.each([
 			{
 				type : 'twitter',
 				options:{
 					button : 'none',
 					text:   '関東ＮＯ．１高収入サイト【はじめての体験入店】は、'+
-						'未経験の誰にでも出来るトーク倶楽部から高級風俗迄、'+
-						'高収入求人や風俗アルバイトを簡単一発検索♪サクッと稼げる♪'+'' +
-						'超高収入が即金可能♪【全て営業届出確認書有良店】だから安心安全です。'
+							'未経験の誰にでも出来るトーク倶楽部から高級風俗迄、'+
+							'高収入求人や風俗アルバイトを簡単一発検索♪サクッと稼げる♪'+'' +
+							'超高収入が即金可能♪【全て営業届出確認書有良店】だから安心安全です。'
 				}
 			},
 			{
@@ -36,8 +57,7 @@
 			}
 		],function(){
 			this.options&&(this.options.url='http://tainyu-navi.com/');
-			console.log($('#sosialbutton'));
-			$('#socialbutton').append($('<div/>').socialbutton(this.type,this.options));
+			elem.append($('<div/>').socialbutton(this.type,this.options));
 		});
-	});
-}());
+		socialbutton.append(elem).append('<hr class="socialbutton-clear-botn" />');
+	})
